6a71576c3ab1864e8fd0d122271565d9a40106bc
AlanWehrliLC <wehrlialan1@gmail.com>
2022-10-11 23:46:44 -0300

    Created the component that is the comments

---
+++ src/Components/Comment/index.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from "phosphor-react"
+import styles from "./styles.module.css"
+
+export function Comment(){
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/AlanWehrliLC.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Alan Wehrli</strong>
+                            <time title="October 10, 2022 at 11:20" dateTime="2022-11-10 11:20:00">1 hours ago</time>
+                        </div>
+
+                        <button title="Deletar comentario">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir 
+                        <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
